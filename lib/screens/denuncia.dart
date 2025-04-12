cf1b62b56623ab920bb0cecf8f4fa7351523f56b
rigelsss <rigel.souza@gmail.com>
2025-04-12 19:03:02 -0300

    feat: link externo

--- lib/screens/denuncia.dart
+++ lib/screens/denuncia.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'webview_screen.dart'; 
 import 'page_denuncia.dart';
 
 class DenunciaPage extends StatelessWidget {
@@ -72,22 +73,40 @@ class DenunciaPage extends StatelessWidget {
                 style: TextStyle(fontSize: 16),
               ),
               const SizedBox(height: 12),
+
               Center(
-                child: SizedBox(
-                  width: screenWidth * 0.9,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFF5F5F5),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const WebViewScreen(
+                          url: 'https://auniao.pb.gov.br/servicos/doe/2024/marco/diario-oficial-26-03-2024-portal.pdf/view',
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    width: screenWidth * 0.9,
+                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF5F5F5),
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: Colors.grey.shade300),
                     ),
                     child: const Text(
                       'Decreto Estadual nº 44.889, de 26 de março de 2024',
-                      style: TextStyle(color: Colors.black),
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
+                      ),
                       textAlign: TextAlign.center,
                     ),
                   ),
                 ),
               ),
+
               const SizedBox(height: 20),
             ],
           ),
