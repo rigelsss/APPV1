c08f84a1386d5a1c45090cf4e352705d3f712507
rigelsss <rigel.souza@gmail.com>
2025-04-20 19:23:12 -0300

    fix: metodo deprecated

--- lib/screens/praias.dart
+++ lib/screens/praias.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pie_chart/pie_chart.dart';
 
