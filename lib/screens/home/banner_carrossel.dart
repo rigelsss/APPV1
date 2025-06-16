b5aa8af8f805873bda12579c2bd932e54a664897
rigelsss <rigel.souza@gmail.com>
2025-06-16 12:35:51 -0300

    fix: remocao de importacao invalida

--- lib/screens/home/banner_carrossel.dart
+++ lib/screens/home/banner_carrossel.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:sudema_app/screens/monumentos/monumentos.dart';
 import 'package:sudema_app/screens/jardim/jardim.dart';
 
