fe7d06b2bb6cace5c148b06956eac0a9acce72ee
Lukas <lukas581800@gmail.com>
2025-06-17 16:31:01 -0300

    fix:cconfirmaçao de saida

--- lib/screens/perfil_page.dart
+++ lib/screens/perfil_page.dart
@@ -281,7 +281,31 @@ class PerfiluserState extends State<Perfiluser> {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton.icon(
-                  onPressed: _logout,
+                  onPressed: () async {
+                    final confirmar = await showDialog<bool>(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Center(
+                          child: const Text('Deseja mesmo sair?',style: TextStyle(fontSize: 22),),
+                        ),
+                        actionsAlignment: MainAxisAlignment.center,
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(false),
+                            child: const Text('Cancelar', style: TextStyle(fontSize: 16),),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: const Text('Sair',style: TextStyle(fontSize: 16),),
+                          ),
+                        ],
+                      )
+                    );
+
+                    if (confirmar == true) {
+                      _logout();
+                    }
+                  },
                   icon: const Icon(Icons.logout, color: Colors.white),
                   label: const Text(
                     'Sair',
@@ -309,6 +333,7 @@ class PerfiluserState extends State<Perfiluser> {
               ),
             ],
           ),
+
         ],
       ),
     );
@@ -342,6 +367,8 @@ class PerfiluserState extends State<Perfiluser> {
       ),
     );
   }
+
+
 
   String formatarCpf(String cpf) {
     final digitsOnly = cpf.replaceAll(RegExp(r'\D'), '');
